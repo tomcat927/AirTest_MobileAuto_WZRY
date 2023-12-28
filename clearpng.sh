779f0f0014175995f0229aca09e236d5bef8acef
cndaqiang <chendq@aliyun.com>
2023-12-28 10:38:33 +0800

    clean png

--- clearpng.sh
+++ clearpng.sh
@@ -1,6 +1,10 @@
+mkdir -p .pngtmp
 for i in $(grep -Eo 'tpl[[:alnum:]_]*.png' object.py)
 do
 #cp ../WZRY_AirtestIDE_emulator/$i .
-cp ../IOS/$i .
+#cp ../IOS/$i .
+cp $i .pngtmp/
 done
+echo rm tpl*png
+echo cp .pngtmp/* .
  
