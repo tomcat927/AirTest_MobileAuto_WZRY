6d2be67c027d49d26dbc15aa3904d11b6b0b2b01
cndaqiang <chendq@aliyun.com>
2024-01-07 21:15:00 +0800

    auto

--- WZRY.node.对战前插入.py
+++ WZRY.node.对战前插入.py
@@ -1,2 +1,14 @@
 #self.触摸对战=True
 #self.标准触摸对战=True
+#组队时顺便完成人机检测
+#if self.组队模式: self.标准触摸对战 = True
+#自动分配对战位置
+#字典位置文件=[]
+#字典位置文件.append("字典.中路.android.var_dict_N.txt")
+#字典位置文件.append("字典.打野.android.var_dict_N.txt")
+#字典位置文件.append("字典.发育.android.var_dict_N.txt")
+#字典位置文件.append("字典.游走.android.var_dict_N.txt")
+#字典位置文件.append("字典.对抗.android.var_dict_N.txt")
+#此步位置文件=(self.runstep+self.mynode)%len(字典位置文件)
+#TimeECHO(self.prefix+f"本步{self.runstep}使用字典文件{字典位置文件[此步位置文件]}")
+#self.Tool.var_dict = self.Tool.read_dict(字典位置文件[此步位置文件])
