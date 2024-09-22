bfbc4b8d859dfa0982592e7fbbab7ac34374e2bf
cndaqiang <who@cndaqiang.ac.cn>
2024-09-22 12:01:03 +0800

    del common

--- tiyanfu.bat
+++ tiyanfu.bat
@@ -1,12 +1,1 @@
 %USERPROFILE%\AppData\Local\anaconda3\python.exe -u tiyanfu.py config.ce.txt
-
-;  config.ce.txt
-;  [client]
-;  # 仅在单进程时有效
-;  mynode = 1
-;  # 多进程时，多进程的数目
-;  totalnode = 1
-;  
-;  LDPlayerdir = D:\GreenSoft\LDPlayer
-;  LINK_dict = {
-;      1: "Android:///127.0.0.1:5557"}
