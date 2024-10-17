5fab9c35b19bb5cf4c69831e6d26860d7b7c920e
cndaqiang <who@cndaqiang.ac.cn>
2024-10-17 12:30:17 +0800

    更新example

--- example/WZRY.node.临时初始化.py
+++ example/WZRY.node.临时初始化.py
@@ -1,24 +1,45 @@
-#self.移动端.重启APP()
+#mklink  WZRY.1.临时初始化.txt WZRY.0.临时初始化.txt 创建软连接
+#ln -s  WZRY.0.临时初始化.txt WZRY.1.临时初始化.txt 创建软连接
+self.devmode=True
+#self.组队模式=False
 #self.登录游戏()
-#self.Tool.timedict["领游戏礼包"] = 0
-#self.Tool.timedict["领营地礼包"] = 0
-#if self.王者营地礼包: self.每日礼包_王者营地()
-#self.每日礼包()
-#self.Tool.removefile(self.临时初始化FILE)
-
-#临时调整对战模式
-#self.进入大厅()
-#self.对战模式="模拟战"
-#self.对战模式="5v5匹配"
-
-#
-#特殊活动
-#王者营地的图标变化
-
-
-#修改虚拟机类型
-#self.移动端.LINK="127.0.0.1:5585"
-#self.LINKport = self.移动端.LINK.split(":")[-1]
-#self.LINKhead = self.移动端.LINK[:-len(self.移动端.LINKport)]
-#self.移动端.mynode=3
-#self.移动端.连接设备()
+#................................................................................
+uptime=23.9
+self.对战时间=[0.1,uptime]
+self.限时组队时间=uptime
+#................................................................................
+#每天组队打0,1,2,...,nstep-1共nstep场，组队5v5匹配
+#每天组队打0,1,2,...,ostep-1共ostep场，单人5v5匹配|模拟战
+nstep=3
+ostep=0  # 为了获得更多的经验，可以将 ostep 设为模拟战 或者 0。
+self.对战模式="5v5匹配" if self.jinristep <  nstep else "模拟战"
+if self.Tool.time_getweek() == 6: self.对战模式 = "5v5匹配" #周日一直打人机模式
+#..................................................................................
+# 组队模式
+if self.新的一天: self.组队模式 = True
+self.组队模式 = self.组队模式 and self.jinristep < nstep
+if not self.组队模式: self.限时组队时间=self.对战时间[0]+0.1
+if not self.组队模式: self.Tool.touchfile(self.无法进行组队FILE)
+#................................................................................
+#结束游戏时的操作
+endgame = self.jinristep >=  nstep+ostep
+if self.jinristep > 1 and not self.组队模式: endgame = True
+if self.mynode >  0: endgame = self.jinristep >=  nstep # 辅助账户退场，节约算力
+if os.path.exists(self.青铜段位FILE): endgame = True # 只刷星耀局
+if endgame: self.对战时间[1]=self.对战时间[0]+0.1
+if endgame and self.mynode == 0: self.Tool.timedict["领营地礼包"] = 0 # 主进程多领一次营地，提高从营地礼包的成功率
+if endgame and self.mynode == 0: self.每日礼包_王者营地()
+#................................................................................
+# 每日任务礼包=False，周末前不领取王者的战令经验，可以额外从营地获取战令经验
+self.每日任务礼包=self.Tool.time_getweek() > 5 # 5 == 周六
+#................................................................................
+self.友情礼包_积分夺宝 = True
+self.友情礼包_皮肤碎片 = True
+self.友情礼包_英雄碎片 = False
+self.友情礼包_铭文碎片 = True
+self.友情礼包_皮肤宝箱 = True
+self.友情礼包_回城宝箱 = True
+self.友情礼包_击败宝箱 = True
+self.活动礼包 = True
+self.祈愿礼包 = False
+self.外置礼包_王者营地 = False
--- example/WZRY.node.对战前插入.py
+++ example/WZRY.node.对战前插入.py
@@ -1,9 +1,9 @@
-#self.触摸对战=True
-#self.标准触摸对战=True
-#组队时顺便完成人机检测
-if self.组队模式 and hour <= self.限时组队时间 and self.jinristep < 4: self.标准触摸对战 = True
-#
-#自动分配对战位置
+# 每天早上前两局，标准模式
+if self.组队模式 and self.jinristep <= 1: self.标准模式 = self.触摸对战 = True
+# 组队的时候不要赢，这样获得的战令经验不到上限，可以额外从营地领经验
+# 周日大概率是在刷友情币等项目，不要开模拟对战
+if self.组队模式 and self.Tool.time_getweek() < 6: self.触摸对战 = True
+
 字典位置文件=[]
 字典位置文件.append("字典.中路.android.var_dict_N.txt")
 字典位置文件.append("字典.打野.android.var_dict_N.txt")
@@ -12,11 +12,11 @@ if self.组队模式 and hour <= self.限时组队时间 and self.jinristep < 4:
 字典位置文件.append("字典.对抗.android.var_dict_N.txt")
 此步位置文件=(self.runstep+self.mynode)%len(字典位置文件)
 TimeECHO(f"本步{self.runstep}使用字典文件{字典位置文件[此步位置文件]}")
-self.Tool.var_dict.update(self.Tool.read_dict(字典位置文件[此步位置文件]))
-#如果新赛季，一些元素的位置变了，要
-#if "今日活跃" in self.Tool.var_dict.keys(): del self.Tool.var_dict["今日活跃"]
-#if "本周活跃1" in self.Tool.var_dict.keys(): del self.Tool.var_dict["本周活跃1"]
-#if "本周活跃2" in self.Tool.var_dict.keys(): del self.Tool.var_dict["本周活跃2"]
-#并更新字典位置
+#当游戏界面改版时，很多位置会变，这里只读入对战的字典
+#self.Tool.var_dict.update(self.Tool.read_dict(字典位置文件[此步位置文件]))
+dictfile=self.Tool.read_dict(字典位置文件[此步位置文件])
+for key in ["参战英雄线路","参战英雄头像"]: self.Tool.var_dict[key]=dictfile[key]
+dictfile.update(self.Tool.var_dict)
+self.Tool.save_dict(dictfile, 字典位置文件[此步位置文件])
 #批量更新字典
 #for 此步位置文件 in range(len(字典位置文件)):self.Tool.var_dict.update(self.Tool.read_dict(字典位置文件[此步位置文件]));self.Tool.save_dict(self.Tool.var_dict, 字典位置文件[此步位置文件])
